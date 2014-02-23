1ede798f8a3bc96f919351416372c22161115252
Johnny Bergström <github@joonix.se>
2014-02-23 17:00:15 +0100

    Slice of positions instead of new type for worm tail
    keep it simple

--- interfaces.go
+++ interfaces.go
@@ -6,16 +6,5 @@ type Movable interface {
 	Channel() chan<- Packet
 	Communicate(Packet) error
 	Kill()
-}
-
-type Attachable interface {
-	// Get the attached
-	Next() Attachable
-	// Attach another
-	Attach(Attachable)
-	// Get next positions as well as our own
 	Positions() []Position
-	// Update position of this
-	// Sets Next() to our old position
-	Follow(Position)
 }
--- worm_test.go
+++ worm_test.go
@@ -4,30 +4,44 @@ import "testing"
 
 func TestWormMovability(t *testing.T) {
 	w := NewWorm()
+	var prevPos *Position
 
 	if d := w.Direction(); d == Unknown {
 		t.Errorf("Expeceted direction to be initialized")
 	}
 
 	testPos := func(p Position) {
-		if p != w.position {
-			t.Error("Wrong position", w.position, "!=", p)
+		if p != w.blocks[0] {
+			t.Error("Wrong position", w.blocks[0], "!=", p)
 		}
+		if prevPos != nil {
+			if w.blocks[1] != *prevPos {
+				t.Errorf(
+					"Expected next blocks in chain to have moved, got: %s instead of %s",
+					w.blocks[1], *prevPos,
+				)
+			}
+		}
+		prevPos = &p
 	}
 
-	originalPos := w.position
+	originalPos := w.blocks[0]
 
 	w.Move(Left)
 	if d := w.Direction(); d != Left {
 		t.Errorf("Unexpected direction", d)
 	}
-	testPos(Position{originalPos.X - 1, w.position.Y})
+	testPos(Position{originalPos.X - 1, w.blocks[0].Y})
+
+	if w.blocks[1] != originalPos {
+		t.Error("Expected next block to be same as original position")
+	}
 
 	w.Move(Right)
 	if d := w.Direction(); d != Right {
 		t.Errorf("Unexpected direction", d)
 	}
-	testPos(Position{originalPos.X, w.position.Y})
+	testPos(Position{originalPos.X, w.blocks[0].Y})
 
 	w.Move(Up)
 	if d := w.Direction(); d != Up {
@@ -58,48 +72,5 @@ func TestCommunicate(t *testing.T) {
 	}
 	if d := w.Direction(); d == Down {
 		t.Error("Should not be able to move in opposite direction")
-	}
-}
-
-func TestAttachable(t *testing.T) {
-	head := &Block{position: Position{X: 1, Y: 1}}
-	tail := &Block{position: Position{X: 0, Y: 0}}
-
-	head.Attach(tail)
-	if w := head.Next(); w != tail {
-		t.Error("Expected tail")
-	}
-
-	positions := head.Positions()
-	if l := len(positions); l != 2 {
-		t.Error("Expected 2 Positions for two attachables got", l)
-	}
-
-	correct := []Position{Position{X: 1, Y: 1}, Position{X: 0, Y: 0}}
-	if positions[0] != correct[0] {
-		t.Error("Wrong position for worm", positions[0])
-	}
-	if positions[1] != correct[1] {
-		t.Error("Wrong position for tail", positions[1])
-	}
-
-	head.Follow(Position{2, 2})
-	correct = []Position{Position{X: 2, Y: 2}, Position{X: 1, Y: 1}}
-	positions = head.Positions()
-	if positions[0] != correct[0] {
-		t.Error("Wrong position for worm", positions[0])
-	}
-	if positions[1] != correct[1] {
-		t.Error("Wrong position for tail", positions[1])
-	}
-}
-
-func TestTail(t *testing.T) {
-	w1 := NewWorm()
-	original := len(Attachable(w1).Positions())
-	w1.AddTail(1)
-
-	if l := len(Attachable(w1).Positions()); l != original+1 {
-		t.Error("Expected +1 Positions for two attachables got total of", l)
 	}
 }
